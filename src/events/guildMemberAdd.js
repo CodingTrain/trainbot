5743462146c7e6bd62bbcc685e16668f09afb45f
Daniel Shiffman <daniel.shiffman@gmail.com>
2020-01-14 17:46:53 -0500

    minor adjustments to welcome language, thoughts?

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const stripIndents = require('../utils');
 exports.run = (bot, member) => {
     member.addRole(bot.config.mutedRole);
     bot.channels.get(bot.config.welcomeChannel).send(stripIndents(`
-        Welcome aboard ${member}! :steam_locomotive: Please read the full code of conduct in ${bot.channels.get(bot.config.rulesChannel)} 
-        and react with an emoji at the end. This will unlock posting permission! Also, we want to learn more about you! Say hello using the template in ${bot.channels.get(bot.config.introductionsChannel)}
+        Welcome aboard ${member}! :steam_locomotive: Please read the Code of Conduct and other welcome messages in ${bot.channels.get(bot.config.rulesChannel)} 
+        and react with an emoji. This will unlock posting permission! Also, the community would love to learn more about you! Introduce yourself with the template in ${bot.channels.get(bot.config.introductionsChannel)}
     `);
 };
