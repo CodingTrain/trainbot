3f4d4c007d40662221bbe26a5a63533e7b504f06
Nola1222 <noahvanboven@gmail.com>
2020-01-14 22:08:31 +0100

    Adding introduction for members

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,3 +1,9 @@
+const stripIndents = require('../utils');
+
 exports.run = (bot, member) => {
     member.addRole(bot.config.mutedRole);
+    bot.channels.get(bot.config.welcomeChannel).send(stripIndents(`
+        Welcome aboard ${member}! :steam_locomotive: Please read the full code of conduct in ${bot.channels.get(bot.config.rulesChannel)} 
+        and react with an emoji at the end. This will unlock posting permission! Also, we want to learn more about you! Say hello using the template in ${bot.channels.get(bot.config.introductionsChannel)}
+    `);
 };
