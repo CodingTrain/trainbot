d3656ffd02f478f86b2083afc953e7afc74995c0
nobobo1234 <noahvanboven@gmail.com>
2020-01-26 15:55:18 +0100

    Fixing type

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -5,5 +5,5 @@ exports.run = (bot, member) => {
     bot.channels.get(bot.config.welcomeChannel).send(stripIndents(`
         Welcome aboard ${member}! :steam_locomotive: Please read the Code of Conduct and other welcome messages in ${bot.channels.get(bot.config.rulesChannel)} 
         and react with an emoji. This will unlock posting permission! Also, the community would love to learn more about you! Introduce yourself with the template in ${bot.channels.get(bot.config.introductionsChannel)}
-    `);
+    `));
 };
