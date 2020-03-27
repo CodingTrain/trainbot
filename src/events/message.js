f6059315fb0dcf43aade7e7806047b6eba17c147
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-03-27 18:07:20 +0100

    Update message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,6 +1,10 @@
 exports.run = (bot, message) => {
     const { prefix } = bot.config;
     if (!message.content.startsWith(prefix)) return;
-
+    if(bot.blacklist.some(a=>message.content.toLowerCase().includes(a.toLowerCase()))){
+        message.delete(500);//In case this is triggered before the message is actually sent, so that way there is no undeletable messages (I have experience with this problem)
+        return message.channel.send(message.author.toString()+" Hi! Please don't make use of obscene/ discriminatory words, thanks!")
+    }
+    
     bot.commands.handleCommand(message);
 };
