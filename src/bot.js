7c7d27b9e4653c7ace245f6b20971caa75f86d92
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-03-27 10:05:06 +0100

    In case of greater case

--- src/bot.js
+++ src/bot.js
@@ -21,7 +21,7 @@ for (const event of events) {
     bot.on(name, (...args) => eventFunc.run(bot, ...args));
 }
 bot.on('message',message=>{
-    if(bot.blacklist.some(a=>message.content.includes(a))){
+    if(bot.blacklist.some(a=>message.content.toLowerCase().includes(a.toLowerCase()))){
         message.delete(500);//In case this is triggered before the message is actually sent, so that way there is no undeletable messages (I have experience with this problem)
         message.channel.send(message.author.toString()+" Hi! Please don't make use of obscene/ discriminatory words, thanks!")
     }
