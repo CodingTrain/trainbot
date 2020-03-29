4a674c7ee6d3dd167ec8565b70e7fc2ab6ee1fd4
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-03-29 19:51:55 +0200

    Update wordBlacklistManager.js

--- src/commands/moderation/wordBlacklistManager.js
+++ src/commands/moderation/wordBlacklistManager.js
@@ -1,9 +1,6 @@
-const { MessageCollector } = require('discord.js');
-const { resolveUser } = require('../../utils');
-
-exports.run = async (bot, msg, args) => {
-    if(bot.wBlacklist.some(w=>msg.toLowerCase().include(w.toLowerCase()))){
-      msg.delete(500);
-      msg.channel.send(`${msg.author.toString()}, Please don't make use of obsene/discriminatory words!`)//To find how many warnings, search "mentions: [user] by:trainbot" using discord search
+exports.run = async (bot, msg) => {
+    if (bot.wBlacklist.some(w => msg.toLowerCase().include(w.toLowerCase()))) {
+        msg.delete(500);
+        msg.channel.send(`${msg.author.toString()}, Please don't make use of obsene/discriminatory words!`);// To find how many warnings, search "mentions: [user] by:trainbot" using discord search
     }
 };
