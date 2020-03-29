9e76432008833d0d9cbfe7a09ad4e993165a8afb
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-03-29 20:05:49 +0200

    Delete this file

--- src/commands/moderation/wordBlacklistManager.js
+++ src/commands/moderation/wordBlacklistManager.js
@@ -1,6 +1,1 @@
-exports.run = async (bot, msg) => {
-    if (bot.wBlacklist.some(w => msg.toLowerCase().include(w.toLowerCase()))) {
-        msg.delete(500);
-        msg.channel.send(`${msg.author.toString()}, Please don't make use of obsene/discriminatory words!`);// To find how many warnings, search "mentions: [user] by:trainbot" using discord search
-    }
-};
+
