10325b37200067ea5becf8a4f916f615e1b21fa0
nobobo1234 <noahvanboven@gmail.com>
2020-03-30 21:33:22 +0200

    Removing blacklist

--- src/bot.js
+++ src/bot.js
@@ -8,15 +8,11 @@ const bot = new Discord.Client();
 const config = JSON.parse(
     fs.readFileSync(path.join(__dirname, '..', 'config.json')),
 );
-const wordBlacklist = JSON.parse(
-    fs.readFileSync(path.join(__dirname, 'words-blacklist.json')),
-);
 
 bot.config = config;
 bot.commands = new Commands(bot);
 bot.logger = new Logger();
 bot.blacklist = new Discord.Collection();
-bot.wBlacklist = wordBlacklist;
 
 const events = fs.readdirSync(path.join(__dirname, 'events'));
 for (const event of events) {
