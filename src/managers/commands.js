76af2e2f1a14e4693faaeeefaf3adfc619ecbc65
informathemusic <39065949+informathemusic@users.noreply.github.com>
2020-04-18 18:23:32 +0200

    Update commands.js

--- src/managers/commands.js
+++ src/managers/commands.js
@@ -93,7 +93,9 @@ class Commands {
             if (command.info.owner && this.bot.config.ownerID !== msg.author.id) {
                 return msg.channel.send(':x: Sorry, only the owner can run this command');
             }
-
+            msg.author.send("Hi!👋\nPlease send your command in the appropriate channel! (<#276366150713999363>)")
+                .catch(()=>{msg.channel.send(msg.member.toString()+", please send your command in the appropriate channel! (<#276366150713999363>)")})
+            msg.delete(500);
             const { permissions } = command.info;
             const { roles } = command.info;
             if (permissions && permissions.some(e => !msg.member.hasPermission(e))) {
