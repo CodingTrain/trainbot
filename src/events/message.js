b7cbea65cbea177479c4b203f8427b77c19195b8
Arnav Sirigere <arnavsirigere@gmail.com>
2020-09-02 19:55:12 +0530

    Triple equals ===

--- src/events/message.js
+++ src/events/message.js
@@ -3,7 +3,7 @@ const { emojis } = require('../constants');
 exports.run = (bot, message) => {
     bot.commands.handleCommand(message);
 
-    if (message.channel.id == bot.config.introductionsChannel) {
+    if (message.channel.id === bot.config.introductionsChannel) {
         message.react(emojis.ct_semicolon);
     }
 };
