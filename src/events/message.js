f3ac696ebababd800134c798f01945e06c1cfac9
Arnav Sirigere <arnavsirigere@gmail.com>
2020-09-06 14:00:13 +0530

    Making requested changes

--- src/events/message.js
+++ src/events/message.js
@@ -1,9 +1,4 @@
-const { emojis } = require('../constants');
-
 exports.run = (bot, message) => {
+    bot.commands.handleReactions(message);
     bot.commands.handleCommand(message);
-
-    if (message.channel.id === bot.config.introductionsChannel) {
-        message.react(emojis.ct_semicolon);
-    }
 };
